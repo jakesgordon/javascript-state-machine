8c4b0414bd8a1c0747407bd1e5be221a240772be
taoqf <tao_qiufeng@126.com>
2017-09-21 15:20:19 +0800

    set umdNamedDefine false

--- dist/state-machine-history.js
+++ dist/state-machine-history.js
@@ -2,7 +2,7 @@
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
 	else if(typeof define === 'function' && define.amd)
-		define("StateMachineHistory", [], factory);
+		define([], factory);
 	else if(typeof exports === 'object')
 		exports["StateMachineHistory"] = factory();
 	else
