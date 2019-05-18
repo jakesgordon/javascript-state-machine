a72fa564b268c9092584d287c0695148e8c6495f
Nelson Francisco <nelson_cunha1991@hotmail.com>
2019-05-18 12:07:47 +0100

    Build output files

--- dist/state-machine.js
+++ dist/state-machine.js
@@ -465,9 +465,13 @@ mixin(JSM.prototype, {
 
   beginTransit: function()          { this.pending = true;                 },
   endTransit:   function(result)    { this.pending = false; return result; },
-  failTransit:  function(result, from, to) {
+  failTransit:  function(result, args) {
+    var from = args[0].from;
+    var to = args[0].to;
+
     if (this.state === to) {
-       this.state = from;
+      this.state = from;
+      plugin.hook(this, 'cancel', args);
     }
     this.pending = false;
     throw result;
@@ -495,11 +499,11 @@ mixin(JSM.prototype, {
     return [ event, result, true ]
   },
 
-  callObserver: function(event, observer, args, from, to) {
+  callObserver: function(event, observer, args) {
     try {
       return observer[event].apply(observer, args);
     } catch (error) {
-      this.failTransit.call(this, error, from, to);
+      this.failTransit.call(this, error, args);
     }
   },
 
@@ -521,15 +525,13 @@ mixin(JSM.prototype, {
       return this.observeEvents(events, args, event, previousResult);
     }
     else {
-      var from = args[0].from;
-      var to = args[0].to;
       var observer = observers.shift();
-      var result = this.callObserver.call(this, event, observer, args, from, to);
+      var result = this.callObserver.call(this, event, observer, args);
       if (result && typeof result.then === 'function') {
         var jsm = this
         return result.then(this.observeEvents.bind(this, events, args, event))
                      .catch(function (error) {
-                       jsm.failTransit.call(jsm, error, from, to);
+                       jsm.failTransit.call(jsm, error, args);
                      });
       }
       else if (result === false) {
